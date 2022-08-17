c150acd0fa21a32c367e77cbcdbcf066a0a44662
VishnuVardhanNarava <vishnu.narava@gmail.com>
2022-08-17 21:29:42 +0530

    files modified

--- scripts/script.js
+++ scripts/script.js
@@ -4,92 +4,66 @@ let regexpdom02 = /@yahoo.com/;
 let regexpdom03 = /@hotmail.com/;
 
 document.getElementById("signupform").addEventListener("input", ()=>{
+    let fname = document.getElementById("firstname").value;
+    let lname = document.getElementById("lastname").value;
+    let emailid = document.getElementById("emailid").value;
     let usrname = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let cpassword = document.getElementById("confirmpassword").value;
-    let emailid = document.getElementById("emailid").value;
+
+    if(regexp.test(fname) || regexp.test(lname)){
+        alert("No special characeters allowed in here");
+    }
+    if(regexp.test(usrname)){
+        alert("No special characters allowed in here")
+    }
+
+    document.getElementById("firstname").value = fname.replace(/[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g, "");
+    document.getElementById("lastname").value = lname.replace(/[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g, "");
+    document.getElementById("emailid").value = emailid.toLowerCase();
+    document.querySelector("#emailid").value = emailid.replace(/[\~\!\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\<\>\?\/]/g, "");
+    document.getElementById("username").value = document.getElementById("username").value.replace(/[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g, "");
+
+    if(!regexp.test(password) && password != ""){
+        document.getElementById("password").style.border = "2px solid red"
+        document.getElementById("confirmpassword").disabled = true;
+    }else if(regexp.test(password) || password == ""){
+        document.getElementById("password").style.border = "0px solid transparent"
+        document.getElementById("confirmpassword").disabled = false;
+    }else{
+        document.getElementById("password").style.border = "0px solid transparent"
+        document.getElementById("confirmpassword").disabled = false;
+    }
+    
+    if(!regexpdom01.test(emailid) && !regexpdom02.test(emailid) && !regexpdom03.test(emailid) && emailid != ""){
+        document.getElementById("emailid").style.border = "2px solid red";
+    }else{
+        document.getElementById("emailid").style.border = "0px solid transparent";
+    }
 
     if(!regexp.test(usrname) && usrname != "" && regexp.test(password) && password == cpassword && regexpdom01.test(emailid) || regexpdom02.test(emailid) || regexpdom03.test(emailid)){
         document.getElementById("submit").disabled = false;
     }else{
         document.getElementById("submit").disabled = true;
     }
+
+    if(cpassword != password && cpassword != ""){
+        document.getElementById("confirmpassword").style.border = "2px solid red";
+    }else{
+        document.getElementById("confirmpassword").style.border = "0px solid transparent";
+    }
 })
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 document.getElementById("signupform").addEventListener("submit", event => {
     event.preventDefault();
     let firstname = document.getElementById("firstname").value;
     let lastname = document.getElementById("lastname").value;
     let usrname = document.getElementById("username").value;
     let cpassword = document.getElementById("confirmpassword").value;
-    console.log(`First Name: ${firstname}`);
-    console.log(`Last Name : ${lastname}`);
-    console.log(`User Name : ${usrname}`);
-    console.log(`Password  : ${cpassword}`);
-})
-
-//validation for firstname and lastname to not take any special characters
-
-document.getElementById("namediv").addEventListener("input", () => {
-    let fname = document.getElementById("firstname").value;
-    let lname = document.getElementById("lastname").value;
-
-    if(regexp.test(fname) || regexp.test(lname)){
-        alert("No special characeters allowed in here");
-    }
-
-    document.getElementById("firstname").value = document.getElementById("firstname").value.replace(/[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g, "");
-    document.getElementById("lastname").value = document.getElementById("lastname").value.replace(/[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g, "");
-})
-
-//To convert any uppercase character into lowercase and to validate the email id's
-
-document.getElementById("emailid").addEventListener("input", () =>{
-    document.getElementById("emailid").value = document.getElementById("emailid").value.toLowerCase();
-    document.querySelector("#emailid").value = document.getElementById("emailid").value.replace(/[\~\!\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\<\>\?\/]/g, "");
-    let eid = document.getElementById("emailid").value;
-
-    if(!regexpdom01.test(eid) && !regexpdom02.test(eid) && !regexpdom03.test(eid) && eid != ""){
-        document.getElementById("emailid").style.border = "2px solid red";
-    }else{
-        document.getElementById("emailid").style.border = "0px solid transparent";
-    }
-})
-
-//validation for username
-
-document.querySelector("#username").addEventListener("input", () => {
-    let usrname = document.getElementById("username").value;
-    if(regexp.test(usrname)){
-        alert("No special characters allowed in here")
-    }
-
-    document.getElementById("username").value = document.getElementById("username").value.replace(/[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g, "");
-})
-
-//To warn the user if there are is no special character included in the password field
-
-document.getElementById("password").addEventListener("input", () => {
-    let pvalue = document.getElementById("password").value;
-    if(!regexp.test(pvalue) && pvalue != ""){
-        document.getElementById("password").style.border = "2px solid red"
-        document.getElementById("confirmpassword").disabled = true;
-    }else if(regexp.test(pvalue) && pvalue != ""){
-        document.getElementById("password").style.border = "0px solid transparent"
-        document.getElementById("confirmpassword").disabled = false;
-    }else{
-        document.getElementById("password").style.border = "0px solid transparent"
-        document.getElementById("confirmpassword").disabled = false;
-    }
-})
-
-//password validation to check if the both password fields are getting matched with each other
-
-document.getElementById("passdiv").addEventListener("input", () => {
-    let pvalue = document.getElementById("password").value;
-    let cpvalue = document.getElementById("confirmpassword").value;
-    if(cpvalue != pvalue && cpvalue != ""){
-        document.getElementById("confirmpassword").style.border = "2px solid red";
-    }else{
-        document.getElementById("confirmpassword").style.border = "0px solid transparent";
-    }
+    alert(`First Name: ${firstname}
+Last Name: ${lastname}
+UserName: ${usrname}
+Password: ${cpassword}`);
 })

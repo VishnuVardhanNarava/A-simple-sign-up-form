75541b8277b8d219cae635bb89cb4c6a03208363
VishnuVardhanNarava <vishnu.narava@gmail.com>
2022-09-11 01:05:07 +0530

    Finalised version-1.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,122 +1,121 @@
-let regexp = /[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?\/]/g;
+const regexp = /[\~\!\@\#\$\%\^\&\*\(\)\_\+\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?\/]/;
 let regexpdom01 = /@gmail.com/;
 let regexpdom02 = /@yahoo.com/;
 let regexpdom03 = /@hotmail.com/;
+let firstname = document.getElementById("firstname").value;
+let lastname = document.getElementById("lastname").value;
+let emailid = document.getElementById("emailid").value;
+let username = document.getElementById("username").value;
+let password = document.getElementById("setpassword").value;
+let cpassword = document.getElementById("confirmpassword").value;
 
-document.getElementById("signupform").addEventListener("input", ()=>{
-    let fname = document.getElementById("firstname").value;
-    let lname = document.getElementById("lastname").value;
-    let emailid = document.getElementById("emailid").value;
-    let usrname = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let cpassword = document.getElementById("confirmpassword").value;
+function warningmessage(para, message, margin){
+    let warn = document.createElement("span");
+    warn.id = "warnspan";
+    warn.textContent = message;
+    warn.style.color = "#C2432E";
+    warn.style.textAlign = "left";
+    warn.style.marginLeft = margin;
+    warn.style.fontSize = "13px";
+    let a = document.getElementById(para);
+    let parentelement = a.parentNode;
+    parentelement.insertBefore(warn, a.nextSibling);
+}
 
-    if(regexp.test(fname) || regexp.test(lname)){
-        alert("No special characeters allowed in here");
-    }
-    if(regexp.test(usrname)){
-        alert("No special characters allowed in here")
-    }
-
-    document.getElementById("firstname").value = fname.replace(/[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g, "");
-    document.getElementById("lastname").value = lname.replace(/[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g, "");
-    document.getElementById("emailid").value = emailid.toLowerCase();
-    document.querySelector("#emailid").value = emailid.replace(/[\~\!\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\<\>\?\/]/g, "");
-    document.getElementById("username").value = document.getElementById("username").value.replace(/[\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\|\;\'\:\"\,\.\<\>\?]/g, "");
+function deletewarningmessage(){
+    let delwarn = document.getElementById("warnspan");
+    delwarn.remove();
+}
 
 
-    if(!regexp.test(usrname) && usrname != "" && regexp.test(password) && password == cpassword && regexpdom01.test(emailid) || regexpdom02.test(emailid) || regexpdom03.test(emailid)){
-        document.getElementById("submit").disabled = false;
-    }else{
-        document.getElementById("submit").disabled = true;
-    }
+////////////////////////////////////Full Name////////////////////////////////////////////
 
-    if(cpassword != password && cpassword != ""){
-        document.getElementById("confirmpassword").style.border = "2px solid red";
-    }else{
-        document.getElementById("confirmpassword").style.border = "0px solid transparent";
-    }
-})
-
-document.getElementById("emailid").addEventListener("change", () =>{
-    let emailid = document.getElementById("emailid").value;
-
-    function createwarn(){
-        const warn0 = document.createElement("span");
-        warn0.id = "warnspan";
-        warn0.textContent = `\u24D8 Enter valid email id`;
-        warn0.style.marginLeft = "-185px";
-        warn0.style.color = "#C2432E";
-        warn0.style.fontSize = "13px"
-        let a = document.getElementById("usernamediv");
-        let parentelement = a.parentNode;
-        parentelement.insertBefore(warn0, a);
-    }
-
-    function deletewarn(){
-        let delwarn0 = document.getElementById("warnspan");
-        delwarn0.remove();
-    }
+document.getElementById("fullname").addEventListener("input", ()=>{
+    let firstname = document.getElementById("firstname").value;
+    let lastname = document.getElementById("lastname").value;
 
     if(document.getElementById("warnspan")){
-        deletewarn();
+        deletewarningmessage();
+    }
+    if(regexp.test(firstname)){
+        warningmessage("firstnamediv", "\u24D8 Special characters are not allowed in here", "-55px");
+    }
+    if(regexp.test(lastname)){
+        warningmessage("lastnamediv", "\u24D8 Special characters are not allowed in here", "-55px")
+    }
+});
+
+
+///////////////////////////////////////Email//////////////////////////////////////////////
+
+document.getElementById("emaildiv").addEventListener("input", () =>{
+    let emailid = document.getElementById("emailid").value;
+
+    if(document.getElementById("warnspan")){
+        deletewarningmessage();
         document.getElementById("emailid").style.border = "0px solid transparent";
     }
 
     if(!regexpdom01.test(emailid) && !regexpdom02.test(emailid) && !regexpdom03.test(emailid) && emailid != ""){
         document.getElementById("emailid").style.border = "2px solid #C2432E";
-        createwarn();
+        warningmessage("emaildiv", "\u24D8 Enter valid email id", "-185px");
     }
 });
 
-document.getElementById("password").addEventListener("change", ()=>{
-    let password = document.getElementById("password").value;
 
-    function createwarn(){
-        let warn1 = document.createElement("span");
-        warn1.id = "warnspan1";
-        warn1.textContent = `\u24D8 password should contain atleast one special character`;
-        warn1.style.color = "#C2432E";
-        warn1.style.marginLeft = "12px"
-        warn1.style.fontSize = "13px"
-        let a = document.getElementById("confirmpassworddiv");
-        let parentelement = a.parentNode;
-        parentelement.insertBefore(warn1, a);
+///////////////////////////////Username/////////////////////////////////////////
+
+document.getElementById("usernamediv").addEventListener("input", ()=>{
+    let username = document.getElementById("username").value;
+
+    if(document.getElementById("warnspan")){
+        deletewarningmessage();
+        document.getElementById("username").style.border = "2px solid transparent";
+    }
+    if(regexp.test(username)){
+        document.getElementById("username").style.border = "2px solid #C2432E";
+        warningmessage("usernamediv", "\u24D8 Special characters are not allowed", "-100px");
     }
 
-    function deletewarn(){
-        let delwarn1 = document.getElementById("warnspan1");
-        delwarn1.remove();
+})
+
+
+//////////////////////////////////////////Password//////////////////////////////////////////////
+
+document.getElementById("password").addEventListener("input", ()=>{
+    let password = document.getElementById("setpassword").value;
+    let cpassword = document.getElementById("confirmpassword").value;
+
+    if(document.getElementById("warnspan")){
+        deletewarningmessage();
+        document.getElementById("setpassword").style.border = "0px solid transparent";
+        document.getElementById("confirmpassword").style.border = "0px solid transparent"
     }
 
     if(!regexp.test(password) && password != ""){
-        document.getElementById("password").style.border = "2px solid #C2432E";
-        document.getElementById("confirmpassword").disabled = true;
-        createwarn();
-    }else if(regexp.test(password) || password == ""){
-        deletewarn();
-        document.getElementById("password").style.border = "0px solid transparent"
-        document.getElementById("confirmpassword").disabled = false;
-    }else{
-        deletewarn();
-        document.getElementById("password").style.border = "0px solid transparent"
-        document.getElementById("confirmpassword").disabled = false;
+        document.getElementById("setpassword").style.border = "2px solid #C2432E";
+        warningmessage("setpassworddiv", "\u24D8 password should contain atleast one special character", "12px");
+    }
+
+    if(cpassword != password && cpassword != ""){
+        document.getElementById("confirmpassword").style.border = "2px solid #C2432E";
+        warningmessage("confirmpassworddiv", "\u24D8 password didn't match", "-170px");
     }
 })
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-document.getElementById("signupform").addEventListener("submit", event => {
+/////////////////////////////////////Submit///////////////////////////////////////////
+
+document.getElementById("signupform").addEventListener("input", ()=>{
+
+    if(!regexp.test(firstname) && firstname != "" && !regexp.test(lastname) && lastname != "" && !regexp.test(username) && username != "" && regexp.test(password) && password != "" && cpassword == password && cpassword != "" && regexpdom01.test(emailid) || regexpdom02.test(emailid) || regexpdom03.test(emailid) && emailid != ""){
+        document.getElementById("submit").disabled = false;
+    }else{
+        document.getElementById("submit").disabled = true;
+    }
+});
+
+document.getElementById("signupform").addEventListener("submit", event=>{
     event.preventDefault();
-    let firstname = document.getElementById("firstname").value;
-    let lastname = document.getElementById("lastname").value;
-    let emailid = document.getElementById("emailid").value;
-    let usrname = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let cpassword = document.getElementById("confirmpassword").value;
-    alert(`First Name: ${firstname}
-Last Name: ${lastname}
-UserName: ${usrname}
-Password: ${cpassword}`);
-    document.getElementById("firstname").value = "";
-})
+    alert("Submitted successfully");
+}); 
